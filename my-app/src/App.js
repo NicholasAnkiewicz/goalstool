9c23b0778c4579b78b25a8c38409afda1aaecc5f
Prady1272 <114194702+Prady1272@users.noreply.github.com>
2022-10-18 22:23:16 -0400

    create goal page (#9)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
- 
-import Login from './pages/Login';
+ import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import ManagerDashboard from './pages/ManagerDashboard'; 
+import NewGoal from './pages/NewGoal';
 
 function App() {
     return (      
@@ -11,6 +11,7 @@ function App() {
                 < Route path='/' element={<Login />} />
                 < Route path='/dashboard' element={<Dashboard />} />
                 < Route path='/manager-dashboard' element={<ManagerDashboard />} />
+                < Route path='/NewGoal' element={<NewGoal />} />
            </Routes>
       </BrowserRouter>
     );
