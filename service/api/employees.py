c5ca7a82732c6b8b456415f195bb6a4b33a99ec6
Saakshaat <saakshaat2001@gmail.com>
2022-10-26 20:46:34 -0400

    add employee route to get single employee

--- service/api/employees.py
+++ service/api/employees.py
@@ -3,6 +3,8 @@ from typing import List
 from datetime import datetime
 
 from fastapi import APIRouter, Depends
+from fastapi.exceptions import HTTPException
+
 from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 
@@ -14,8 +16,16 @@ import schemas
 employees_router = APIRouter()
 
 @employees_router.get("/employees", response_model=List[schemas.Employee])
-async def get_users(sess: Session=Depends(get_db)):
+async def get_employees(sess: Session=Depends(get_db)):
     return sess.query(models.Employee).all()
+
+@employees_router.get("/employees/{id}", response_model=schemas.Employee)
+async def get_employee(id: int, sess: Session=Depends(get_db)):
+    employee = sess.query(models.Employee).get(id)
+    if employee:
+        return employee
+    else:
+        raise HTTPException(404, detail=f"Employee with id {id} not found")
 
 @employees_router.get("/employees/demo", response_model=schemas.Employee)
 async def seed_test_employee(sess: Session=Depends(get_db)):
