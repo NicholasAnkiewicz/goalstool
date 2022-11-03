464dfc894bca3ed75d0e20e9ca609c5c9ae88780
Arya <65263304+arcpool@users.noreply.github.com>
2022-11-02 23:02:17 -0400

    added Authentication model, schema, and API. (#20)

---
+++ service/api/auth_end.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+from typing import List
+from datetime import datetime
+
+from fastapi import APIRouter, Depends
+from fastapi.exceptions import HTTPException
+
+from fastapi.encoders import jsonable_encoder
+from sqlalchemy.orm import Session
+
+from . import get_db
+
+import models
+import schemas
+
+auth_router = APIRouter()
+
+@auth_router.post("/auth", response_model=schemas.AuthEnd)
+async def auth(credentials: schemas.AuthPost, sess: Session=Depends(get_db)):
+    return sess.query(models.AuthEnd).filter() ## needs change
+
+# @auth_router.get("/employee/{id}", response_model=schemas.Employee)
+# async def get_employee(id: int, sess: Session=Depends(get_db)):
+#     employee = sess.query(models.Employee).get(id)
+#     if employee:
+#         return employee
+#     else:
+#         raise HTTPException(404, detail=f"Employee with id {id} not found")
+
+# @employees_router.get("/employees/demo", response_model=schemas.Employee)
+# async def seed_test_employee(sess: Session=Depends(get_db)):
+#     test_employee = models.Employee(
+#                 first_name="Saakshaat",
+#                 last_name="Singh",
+#                 employee_id = "UKG123",
+#                 email="saakshaatsin@umass.edu",
+#                 company_id="UKG",
+#                 company_name="UKG",
+#                 position_title="Test User",
+#                 is_manager=False,
+#                 current=True,
+#                 manager_id=123,
+#                 password="easypeesylemonsqueezy"
+#                 )
+
+#     sess.add(test_employee)
+#     sess.commit()
+
+#     sess.refresh(test_employee) # to add id and DB metadata to test_employee for use in response
+#     return test_employee
+
