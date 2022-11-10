6a14fe591fc1aaa025b4e4173d7e5a7be9814b7a
ilmin <choim426@gmail.com>
2022-11-10 18:46:24 -0500

    Edit api/auth_end.py

--- service/api/auth_end.py
+++ service/api/auth_end.py
@@ -17,40 +17,6 @@ auth_router = APIRouter()
 
 @auth_router.post("/auth", response_model=schemas.AuthEnd)
 async def auth(credentials: schemas.AuthPost, sess: Session=Depends(get_db)):
-<<<<<<< HEAD
-    return sess.query(models.AuthEnd).filter() ## needs change
-
-# @auth_router.get("/employee/{id}", response_model=schemas.Employee)
-# async def get_employee(id: int, sess: Session=Depends(get_db)):
-#     employee = sess.query(models.Employee).get(id)
-#     if employee:
-#         return employee
-#     else:
-#         raise HTTPException(404, detail=f"Employee with id {id} not found")
-
-# @employees_router.get("/employees/demo", response_model=schemas.Employee)
-# async def seed_test_employee(sess: Session=Depends(get_db)):
-#     test_employee = models.Employee(
-#                 first_name="Saakshaat",
-#                 last_name="Singh",
-#                 employee_id = "UKG123",
-#                 email="saakshaatsin@umass.edu",
-#                 company_id="UKG",
-#                 company_name="UKG",
-#                 position_title="Test User",
-#                 is_manager=False,
-#                 current=True,
-#                 manager_id=123,
-#                 password="easypeesylemonsqueezy"
-#                 )
-
-#     sess.add(test_employee)
-#     sess.commit()
-
-#     sess.refresh(test_employee) # to add id and DB metadata to test_employee for use in response
-#     return test_employee
-
-=======
     auth = sess.query(models.AuthEnd).filter(models.AuthEnd.username == credentials.username, models.AuthEnd.password == credentials.password).first()
     if auth:
         return auth
